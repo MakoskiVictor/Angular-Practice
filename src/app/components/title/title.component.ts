f71712fdf99b9233fa964205d26083cd6105aab6
MakoskiVictor <makoski.ed@gmail.com>
2023-09-11 20:46:49 -0300

    add inputs to title

--- src/app/components/title/title.component.ts
+++ src/app/components/title/title.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, /* OnInit */ } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.scss']
 })
-export class TitleComponent {
+export class TitleComponent /* implements OnInit */ {
+  titleRendered: boolean
+  @Input() titleText = 'This is the Title'
+  constructor() {
+    this.titleRendered = true
+  }
 
+
+  // ngOnInit(): void {
+  //   //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+  //   //Add 'implements OnInit' to the class.
+  //   alert('Me he inicido')
+  // }
 }
